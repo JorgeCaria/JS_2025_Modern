8774db6cc667c7b2fd5ba49478f6414eacaed1e6
Jorge Caria <jorgefcaria@gmail.com>
2025-02-11 12:43:33 +0100

    Introducción a variables y comentarios

--- 01 - Fundamentos/app.js
+++ 01 - Fundamentos/app.js
@@ -1,2 +1,10 @@
 
-console.log(console.log('Hola mundo desde el app.js'));
+// console.log(console.log('Hola mundo desde el app.js'));
+
+let a = 10, 
+    b = 20, 
+    c = 10,
+    d = 10,
+    x = a + b;
+
+console.log( x );
