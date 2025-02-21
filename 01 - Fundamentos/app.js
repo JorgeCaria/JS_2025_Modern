29be10d353369699141f8943ce8ec786f0316f6c
JorgeDev <93055253+JorgeCaria@users.noreply.github.com>
2025-02-21 11:38:22 +0100

    Debugging

--- 01 - Fundamentos/app.js
+++ 01 - Fundamentos/app.js
@@ -1,17 +1,21 @@
 
-// console.log(console.log('Hola mundo desde el app.js'));
+console.log(console.log('Hola mundo desde el app.js'));
 
 let a = 10, 
     b = 20, 
     c = 'Hola ',
     d = 'Spiderman',
     x = a + b;
-
+ 
 const saludo = c + d;
 
+console.log('%c Mis variables', 'color:blue; font-weight:bold')
+console.log( {a} );
+console.log( {b} );
+console.log( {c} );
 
-// console.log('%c Mis variables', 'color:blue; font-weight:bold')
-// console.log( {a} );
-// console.log( {b} );
-// console.log( {c} );
-// console.table({a,b,c,d,x});
+
+c = "Hola de nuevo";
+
+
+console.table({a,b,c,d,x});
