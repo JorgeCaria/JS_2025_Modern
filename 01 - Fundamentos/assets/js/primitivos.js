337ea8874becae1c5279c696c7fb5c2e15da8a18
JorgeDev <93055253+JorgeCaria@users.noreply.github.com>
2025-02-27 17:41:47 +0100

    Introducción a primitivos

--- 01 - Fundamentos/assets/js/primitivos.js
+++ 01 - Fundamentos/assets/js/primitivos.js
@@ -1,4 +1,35 @@
 let nombre = 'Peter Parker';
-
-
 console.log( 'Nombre: ' + nombre );
+
+nombre = 'Ben Parker';
+console.log( 'Nombre: ' + nombre );
+
+nombre = "Tía May";
+nombre = `Tía May`;
+
+
+console.log( typeof nombre );
+nombre = 123;
+console.log( typeof nombre );
+
+
+let esMarvel = false;
+console.log( typeof esMarvel);
+
+let edad = 33.0001;
+console.log( typeof edad);
+
+let superPoder;
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log(typeof null);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log(typeof symbol1);
+console.log(typeof symbol2);
+
+console.log( symbol1 === symbol2 );
+
+
